a853cd3596cd368aa60ec10c73072c4be2d794d7
James Robert Perih <james.perih@brewninja.net>
2018-07-31 10:13:36 -0600

    Cleaning Up

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,8 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React+MobX+TypeScript</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <div style={{ display: 'flex' }}>
           <LeftBrain
             title="Left Brain"
